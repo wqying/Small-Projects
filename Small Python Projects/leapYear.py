804df61008f9520bd28870d534141af566225b1d
Qian Ying Wong <qywong@uci.edu>
2025-01-29 09:41:01 -0800

    Finished leap year project

---
+++ Small Python Projects/leapYear.py
@@ -0,0 +1,13 @@
+def is_leap_year(year):
+    # Write your code here.
+    # Don't change the function name.
+
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 == 0:
+            return True
+        elif year % 100 != 0:
+            return True
+        else:
+            return False
+    else:
+        return False
