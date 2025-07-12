c48f7d5f2bba1dcf95a8c7d58dfe481d3c06b9fb
wqying <qywong@uci.edu>
2025-07-12 15:21:00 -0500

    some basic html

--- Small Python Projects/journal/ui.py
+++ Small Python Projects/journal/ui.py
@@ -2,13 +2,14 @@
 Handles the user interface of the project using tkinter.
 """
 
+import tkinter as tk
 from tkinter import *
 
 
-main_window = Tk()
+main_window = tk.Tk(screenName="main")
 main_window.title("Welcome to...!")
-title = Label(main_window, text="Ying's Game of Life :3")
+title = tk.Label(main_window, text="Ying's Game of Life xD")
 title.pack()
-button = Button(main_window, text="CLOSE BITCH", command=main_window.destroy)
+button = tk.Button(main_window, text="CLOSE BITCH", command=main_window.destroy)
 button.pack()
 main_window.mainloop()
